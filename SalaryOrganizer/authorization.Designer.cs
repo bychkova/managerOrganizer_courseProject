2da7ab6260841010e25c59f203deb930c914864b
bychkova <mar.bychkova@gmail.com>
2023-01-22 15:52:13 +0300

    Authorization functionality

--- SalaryOrganizer/authorization.Designer.cs
+++ SalaryOrganizer/authorization.Designer.cs
@@ -31,53 +31,82 @@
             this.loginField = new System.Windows.Forms.TextBox();
             this.passwordField = new System.Windows.Forms.TextBox();
             this.authoBtn = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             this.SuspendLayout();
             // 
             // loginField
             // 
             this.loginField.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.loginField.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.loginField.Location = new System.Drawing.Point(38, 97);
+            this.loginField.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.loginField.Location = new System.Drawing.Point(195, 90);
             this.loginField.Name = "loginField";
             this.loginField.Size = new System.Drawing.Size(241, 29);
             this.loginField.TabIndex = 0;
-            this.loginField.Text = "Имя пользователя";
-            this.loginField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // passwordField
             // 
             this.passwordField.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.passwordField.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.passwordField.Location = new System.Drawing.Point(38, 158);
+            this.passwordField.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.passwordField.Location = new System.Drawing.Point(195, 146);
             this.passwordField.Name = "passwordField";
             this.passwordField.PasswordChar = '*';
             this.passwordField.Size = new System.Drawing.Size(241, 29);
             this.passwordField.TabIndex = 1;
-            this.passwordField.Text = "Пароль";
-            this.passwordField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // authoBtn
             // 
             this.authoBtn.Depth = 0;
-            this.authoBtn.Location = new System.Drawing.Point(38, 222);
+            this.authoBtn.Location = new System.Drawing.Point(16, 201);
             this.authoBtn.MouseState = MaterialSkin.MouseState.HOVER;
             this.authoBtn.Name = "authoBtn";
             this.authoBtn.Primary = true;
-            this.authoBtn.Size = new System.Drawing.Size(241, 38);
+            this.authoBtn.Size = new System.Drawing.Size(420, 38);
             this.authoBtn.TabIndex = 2;
             this.authoBtn.Text = "войти";
             this.authoBtn.UseVisualStyleBackColor = true;
+            this.authoBtn.Click += new System.EventHandler(this.authoBtn_Click);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(12, 90);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(177, 24);
+            this.materialLabel1.TabIndex = 3;
+            this.materialLabel1.Text = "Имя пользователя";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(12, 146);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(76, 24);
+            this.materialLabel2.TabIndex = 4;
+            this.materialLabel2.Text = "Пароль";
             // 
             // authorization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 301);
+            this.ClientSize = new System.Drawing.Size(452, 260);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
             this.Controls.Add(this.authoBtn);
             this.Controls.Add(this.passwordField);
             this.Controls.Add(this.loginField);
-            this.MaximumSize = new System.Drawing.Size(322, 301);
-            this.MinimumSize = new System.Drawing.Size(322, 301);
+            this.MaximumSize = new System.Drawing.Size(452, 260);
+            this.MinimumSize = new System.Drawing.Size(452, 260);
             this.Name = "authorization";
             this.Sizable = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -92,5 +121,7 @@
         private System.Windows.Forms.TextBox loginField;
         private System.Windows.Forms.TextBox passwordField;
         private MaterialSkin.Controls.MaterialRaisedButton authoBtn;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
     }
 }
--- SalaryOrganizer/authorization.cs
+++ SalaryOrganizer/authorization.cs
@@ -1,5 +1,7 @@
-﻿using MaterialSkin;
+﻿using Connection_DB;
+using MaterialSkin;
 using MaterialSkin.Controls;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,6 +9,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,12 +17,52 @@ namespace SalaryOrganizer
 {
     public partial class authorization : MaterialForm
     {
+
+        Thread authToMain;
         public authorization()
         {
             InitializeComponent();
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
         }
-
+        connection con = new connection();
+        private void authoBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (loginField.Text != "" && passwordField.Text != "")
+                {
+                    con.Open();
+                    string query = "SELECT user_name, pass_word FROM users WHERE user_name ='" + loginField.Text + "' AND pass_word ='" + passwordField.Text + "'";
+                    MySqlDataReader row;
+                    row = con.ExecuteReader(query);
+                    if (row.HasRows)
+                    {
+                        this.Close();
+                        authToMain = new Thread(openMain);
+                        authToMain.SetApartmentState(ApartmentState.STA);
+                        authToMain.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь не найден", "Сообщение");
+                    }
+                    con.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Пустое поле логин или пароль", "Сообщение");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка соединения", "Сообщение");
+            }
+        }
+        
+        public void openMain(object obj)
+        {
+            Application.Run(new Form1());
+        }
     }
 }
